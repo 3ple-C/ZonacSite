c38813dd0f99509888ca4b059e2de8786d9f7670
3ple-C <triplec871@gmail.com>
2024-06-20 14:20:42 +0100

    Athentication and order functions

---
+++ main-zoniac/js/login.js
@@ -0,0 +1,22 @@
+// Toggle login
+let signUp = document.getElementById('signup');
+let login = document.getElementById('login');
+
+let createAccount = () => {
+    if (signUp.style.display = 'none') {
+        signUp.style.display = 'block';
+        login.style.display = 'none';
+    } else if (signUp.style.display = 'block') {
+        signUp.style.display = 'none';
+        login.style.display = 'block';
+    }
+
+    console.log('block')
+}
+
+let signIn = () => {
+    if (signUp.style.display = 'block') {
+        signUp.style.display = 'none';
+        login.style.display = 'block';
+    }
+}
